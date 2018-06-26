00507809e8df5ce8e6eed7bc3279ed3429eeefbf
roei baruch <roei46@yahoo.com>
2018-06-26 13:11:24 +0300

    added dependency

--- PodMasterTesting/Classes/RandomGenerator.swift
+++ PodMasterTesting/Classes/RandomGenerator.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Alamofire
 
 public class RandomRoei {
     
@@ -17,5 +18,21 @@ public class RandomRoei {
     
     public static func integer() -> Int {
         return Int(arc4random())
+    }
+    
+    public static func requestLogin(name: String, password: String, success:@escaping () -> Void, failure:@escaping (Error) -> Void) {
+        Alamofire.request("http://www.theaudiodb.com/api/v1/json/195003/track-top10.php?s=iron%20maiden").responseJSON { (responseObject) -> Void in
+            if responseObject.result.isSuccess {
+                print("login succsess")
+                print(responseObject.description)
+                success()
+            }
+            
+            if responseObject.result.isFailure {
+                let error : Error = responseObject.result.error!
+                print("login failed")
+                failure(error)
+            }
+        }
     }
 }
